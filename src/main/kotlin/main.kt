437ef471f8e3977ac8c2d135c725c8f16bacde51
Diana Letko <diana.letko@bk.ru>
2024-10-31 09:59:27 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,45 @@
+
+fun main(){
+    val typeOfCard = "Мир"
+    val previousTransfersMonth = 0
+    val transfer = 150_000
+
+    val commission = calculateCommission(transfer, typeOfCard, previousTransfersMonth)
+
+    println(commission)
+}
+
+fun calculateCommission(transfer: Int, typeOfCard:String, previousTransfersMonth:Int ) {
+   var checkLimitMonth = false
+    if(previousTransfersMonth<=600_000){
+        checkLimitMonth = true
+    }
+
+    var checkLimitDay = false
+    if (transfer<= 150_000){
+        checkLimitDay = true
+    }
+
+    if (checkLimitMonth&&checkLimitDay) {
+        var finalTransfer = 0
+        if (transfer + previousTransfersMonth >= 600_000) {
+            finalTransfer = 600_000 - previousTransfersMonth
+        } else {
+            finalTransfer = transfer
+        }
+val commission = when(typeOfCard){
+    "Mastercard" -> if (finalTransfer>75_000) (finalTransfer-75_000)*0.006 + 20 else 0
+        "Visa" -> if (finalTransfer*0.0075 > 35) finalTransfer* 0.0075 else 35
+            "Мир" -> 0
+                else -> false
+
+}
+        println("Клмиссия составит $commission рублей")
+    } else {
+println("Превышен один из лимитов")
+    }
+}
+
+
+
+
